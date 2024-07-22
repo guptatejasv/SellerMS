730aab936a826517906ea07f82d7963f8dc275b9
Tejasv Kumar <guptatejasv86086@gmail.com>
2024-07-22 14:37:31 +0530

    Seller MS commit 1

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
