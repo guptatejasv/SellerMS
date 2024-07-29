c58a43c1a119d5c3302d7cdc331df52b4761d24d
Tejasv Kumar <guptatejasv86086@gmail.com>
2024-07-29 19:47:40 +0530

    Completed create Product Api

--- src/controllers/seller.addProduct.ts
+++ src/controllers/seller.addProduct.ts
@@ -3,6 +3,20 @@ import { Product } from "../models/seller.model";
 
 export const addProduct = async (req: Request, res: Response) => {
   try {
+    const { name, description, price, category } = req.body;
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      category,
+    });
+    res.status(201).json({
+      status: "success",
+      message: "Product Added Successfully..",
+      data: {
+        product,
+      },
+    });
   } catch (err) {
     res.status(400).json({
       status: "fail",
--- src/models/seller.model.ts
+++ src/models/seller.model.ts
@@ -1,9 +1,39 @@
 import { Schema, Document, model } from "mongoose";
 
-export interface IAuth extends Document {}
+export interface IAuth extends Document {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  stock?: number;
+  discount?: number;
+}
 
 const AuthSchema: Schema = new Schema(
-  {},
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    stock: {
+      type: Number,
+    },
+    discount: {
+      type: Number,
+    },
+  },
   {
     timestamps: true,
     versionKey: false,
